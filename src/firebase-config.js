9f57a7db99c4b87876688b50af2f64785c204c89
Kayla Lin <70559748+kayla-lin@users.noreply.github.com>
2022-06-21 23:23:04 -0400

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,13 +4,7 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAsM_DgLzaRkZbpzHXKPBPAr91AKhyH7CI",
-  authDomain: "aboutu-5435b.firebaseapp.com",
-  projectId: "aboutu-5435b",
-  storageBucket: "aboutu-5435b.appspot.com",
-  messagingSenderId: "144538692762",
-  appId: "1:144538692762:web:600ab1e2824fe6b59e4323",
-  measurementId: "G-8M0RRR57E4"
+//  Your API key here 
 };
 
 // Initialize Firebase
